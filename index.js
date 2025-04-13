bee742e3d060a1794b64dadf5705ba0ddc81d876
Kalintz21 <polaquinho.exe@gmail.com>
2025-04-13 02:50:22 -0300

    index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["DK Guardian 👑"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
